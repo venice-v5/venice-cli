43f9f828c74665bce445825a124aec684fa1afae
fibonacci <faisal@fibn.cc>
2025-10-27 16:24:46 +0300

    feat: implement update command

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,16 +1,18 @@
 use std::path::PathBuf;
 
 use clap::Parser;
+use miette::IntoDiagnostic;
 use shared::{
     BUILD_DIR,
     build::build,
     errors::CliError,
-    manifest::find_manifest,
+    manifest::{MANIFEST_NAME, find_manifest},
     run::run,
-    runtime,
+    runtime::{self, latest_version},
     terminal::terminal,
     upload::{open_connection, upload},
 };
+use toml_edit::{DocumentMut, Formatted, Item, Value};
 use vex_v5_serial::protocol::cdc2::file::FileExitAction;
 
 #[derive(Debug, Clone, clap::ValueEnum)]
@@ -53,7 +55,10 @@ enum Venice {
         #[arg(long = "directory", short = 'C')]
         dir: Option<PathBuf>,
     },
-    Update,
+    Update {
+        #[arg(long = "directory", short = 'C')]
+        dir: Option<PathBuf>,
+    },
 }
 
 fn clean(dir: Option<PathBuf>) -> miette::Result<()> {
@@ -63,6 +68,28 @@ fn clean(dir: Option<PathBuf>) -> miette::Result<()> {
     };
 
     std::fs::remove_dir_all(manifest_dir.join(BUILD_DIR)).map_err(CliError::Io)?;
+
+    Ok(())
+}
+
+async fn update(dir: Option<PathBuf>) -> miette::Result<()> {
+    let manifest_path = match dir {
+        Some(dir) => dir.join(MANIFEST_NAME),
+        None => find_manifest(None)?,
+    };
+
+    let manifest_src = tokio::fs::read_to_string(&manifest_path)
+        .await
+        .map_err(CliError::Io)?;
+    let mut doc = manifest_src.parse::<DocumentMut>().into_diagnostic()?;
+    let latest_version = latest_version(&reqwest::Client::new()).await?;
+    let version_string = latest_version.to_string();
+
+    doc["project"]["venice-version"] = Item::Value(Value::String(Formatted::new(version_string)));
+
+    tokio::fs::write(manifest_path, doc.to_string())
+        .await
+        .map_err(CliError::Io)?;
 
     Ok(())
 }
@@ -82,7 +109,7 @@ async fn main() -> miette::Result<()> {
         }
         Venice::Terminal => terminal(&mut open_connection().await?).await?,
         Venice::Run { dir } => run(dir).await?,
-        Venice::Update => todo!(),
+        Venice::Update { dir } => update(dir).await?,
     };
 
     Ok(())
