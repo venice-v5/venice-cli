dd0c77618d4e7b904abd44a89a560ec95017e93d
fibonacci <faisal@fibn.cc>
2025-10-31 01:10:34 +0300

    chore: update async-await test api

--- tests/async-await/src/__init__.py
+++ tests/async-await/src/__init__.py
@@ -1,11 +1,12 @@
 import vasyncio
+from vasyncio import Sleep
 
 async def sleep():
     await vasyncio.sleep_ms(1000)
 
 async def main():
     print("sleeping")
-    await sleep()
+    await Sleep(millis=1000)
     print("slept for 1000 ms")
 
 event_loop = vasyncio.new_event_loop()
